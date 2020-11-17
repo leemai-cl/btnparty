17d56edbbe7b2446438374df1e425ac264e20caa
Lee Mai <ducmai17@gmail.com>
2020-11-17 17:18:01 -0500

    more buttons and cleanup

--- js.js
+++ js.js
@@ -1,7 +1,43 @@
 
 
 $(document).ready(function(){
-  $("button").click(function(){
+  $(".btntog").click(function(){
     $("p").fadeToggle(500);
   });
 });
+
+let n=0
+let x=0
+
+
+while (n<3){
+    n++
+    x += n;
+}
+
+function btnmath() {
+
+    document.getElementById("btnnum").innerHTML = x;
+}
+
+
+function btnFunction() {
+    var demo2 = document.getElementById("demo2")
+    var button = document.createElement("BUTTON")
+    button.innerHTML = "___";
+    demo.appendChild(button)
+    
+    console.log(button)
+}
+
+function myFunction5() {
+    document.getElementById("demo").innerHTML = "";
+}
+
+window.onload=function(){
+document.getElementById("demo").addEventListener("click", myFunction);
+}
+function myFunction() {
+  document.getElementById("demo").innerHTML = "you've clicked this ";
+}
+
