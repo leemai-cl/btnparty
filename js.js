2b22011b7eff623b3777afe603bf5bfc9b464d8b
Lee Mai <ducmai17@gmail.com>
2020-11-19 16:56:11 -0500

    comments in file

--- js.js
+++ js.js
@@ -1,39 +1,23 @@
 
-
+//this is a button that shows and hides content
 $(document).ready(function(){
   $(".btntog").click(function(){
     $("p").fadeToggle(500);
   });
 });
 
+//simple while loop that displays the number 6, not finished
 let n=0
 let x=0
-
-
 while (n<3){
     n++
     x += n;
 }
-
 function btnmath() {
-
     document.getElementById("btnnum").innerHTML = x;
 }
 
-
-// function btnFunction() {
-//     // var demo2 = document.getElementById("demo2")
-//     var button = document.createElement("BUTTON")
-//     button.innerHTML = "___";
-//     demo.appendChild(button)
-    
-//     console.log(button)
-// }
-
-// function myFunction5() {
-//     document.getElementById("demo").innerHTML = "";
-// }
-
+//clickable text that changes the text
 window.onload=function(){
 document.getElementById("demo").addEventListener("click", myFunction);
 }
@@ -41,7 +25,8 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "you've clicked this ";
 }
 
- 
+//for loop that uses form value to create # of buttons
+//also should display an alert is form value is empty
 var i;
   
 function btnloop() { 
@@ -53,7 +38,7 @@ function btnloop() {
    btnFunction();
  }
 }
-
+//function that creates single useless buttons
 function btnFunction() {
     var demo2 = document.getElementById("demo2")
     var button = document.createElement("BUTTON")
@@ -62,17 +47,16 @@ function btnFunction() {
     
     console.log(button)
 }
-
+//clears useless buttons on page
  function myFunction5() {
      document.getElementById("demo2").innerHTML = "buttons appear here ->";
 }
-
-function validateForm() {
-    let x = document.forms["myForm"]["btnform"].value;
-    if (x == "") {
-        alert("how many buttons do you want?");
-        return false;
+//pulls form number value, this function might not be needed anymore
+// function validateForm() {
+//     let x = document.forms["myForm"]["btnform"].value;
+//     if (x == "") {
+//         alert("how many buttons do you want?");
+//         return false;
         
-    }
+//     }
 
-}
